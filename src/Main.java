c05289116bcd7d2df6dc6dcdf027f598a6411664
liqiang <75697523@qq.com>
2019-04-01 11:23:31 +0800

    添加分支后测试

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     private String addr;
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("test one");
     }
 }
